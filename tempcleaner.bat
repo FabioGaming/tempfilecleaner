be197da0911a106825b91fc45282cbe471145a73
Fabio Gaming <fabio.furch@gmail.com>
2021-10-29 02:47:24 +0200

    TempCleaner file

---
+++ tempcleaner.bat
@@ -0,0 +1,13 @@
+@echo off
+color b
+title TempCleaner
+echo Cleanign temp...
+timeout 3 /nobreak >nul
+del C:\Windows\Temp\* /f /s /q
+del %temp%\* /f /s /q
+timeout 3 /nobreak >nul
+echo Cleaned all temp data.
+timeout 1 /nobreak >nul
+echo Closing Program.
+timeout 1 /nobreak >nul
+exit
